a9636d37d3df0403d3b4ef5edbeeaf10a39e21e6
Simon Swartzman <sswartzman@activecampaign.com>
2022-10-19 22:04:33 -0500

    Revert "Zoom in on top of image when processing"
    This reverts commit 7f47b348ddf8e8a7a98f75207a71f924f66c89d2.

--- src/components/processor.tsx
+++ src/components/processor.tsx
@@ -16,15 +16,13 @@ const TOTAL_HEIGHT = 480;
 const TOP_WIDTH_WARP = 0.5;
 const START_TOP_LEFT = (TOTAL_WIDTH * TOP_WIDTH_WARP) * 1/2;
 const START_TOP_RIGHT = (TOTAL_WIDTH * TOP_WIDTH_WARP) * 3/2;
-const ZOOMED_WIDTH = START_TOP_RIGHT - START_TOP_LEFT;
+const ZOOMED_WIDTH = START_TOP_RIGHT- START_TOP_LEFT;
 const BOTTOM_WIDTH_WARP = 0.5;
-const BOTTOM_ZOOMED_HEIGHT = TOTAL_HEIGHT * BOTTOM_WIDTH_WARP;
-
+const BOTTOM_WARPED_HEIGHT = TOTAL_HEIGHT * BOTTOM_WIDTH_WARP;
 const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) => {
 
   const [offscreenContext, setOffscreenContext] = useState<CanvasRenderingContext2D | null>(null);
   const [zoomedContext, setZoomedContext] = useState<CanvasRenderingContext2D | null>(null);
-  const [initialZoomedContext, setInitialZoomedContext] = useState<CanvasRenderingContext2D | null>(null);
   const rafRef = React.useRef<number | undefined>();
   const previousTimeRef = React.useRef<number>();
 
@@ -35,7 +33,6 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
 
   const offscreenCanvas = useRef<HTMLCanvasElement>(null);
   const zoomedCanvas = useRef<HTMLCanvasElement>(null);
-  const initialZoomedCanvas = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     if (offscreenCanvas.current) {
       setOffscreenContext(offscreenCanvas.current.getContext('2d', { alpha: false }))
@@ -59,23 +56,6 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
     }
   }, [zoomedCanvas]);
 
-  useEffect(() => {
-    if (initialZoomedCanvas.current) {
-      const tempInitialZoomedContext = initialZoomedCanvas.current.getContext('2d', { alpha: false });
-      if (tempInitialZoomedContext) {
-        tempInitialZoomedContext.imageSmoothingEnabled = true;
-        // @ts-ignore
-        tempInitialZoomedContext.mozImageSmoothingEnabled = true;
-        // @ts-ignore
-        tempInitialZoomedContext.webkitImageSmoothingEnabled = true;
-        // @ts-ignore
-        tempInitialZoomedContext.msImageSmoothingEnabled = true;
-  
-        setInitialZoomedContext(tempInitialZoomedContext)  
-      }
-    }
-  }, [initialZoomedCanvas]);
-
   const process = (
     time: number,
     offscreenContext: CanvasRenderingContext2D,
@@ -86,41 +66,29 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
       // TODO: move more of this into the Converter
       offscreenContext.drawImage(video, 0, 0, width, height);
 
-      if (offscreenCanvas && offscreenCanvas.current && initialZoomedContext && initialZoomedCanvas && initialZoomedCanvas.current) {
-        initialZoomedContext.drawImage(
-          offscreenCanvas.current,
-          0,
-          0,
-          TOTAL_WIDTH,
-          BOTTOM_ZOOMED_HEIGHT,
-          0,
-          0,
-          TOTAL_WIDTH,
-          TOTAL_HEIGHT);
+      // jsfeat projection transform
+      const tempImageData = offscreenContext.getImageData(0, 0, width, height);
 
-        // jsfeat projection transform
-        const tempImageData = initialZoomedContext.getImageData(0, 0, TOTAL_WIDTH, TOTAL_HEIGHT);
+      // Make image grayscale for easier processing
+      jsfeat.imgproc.grayscale(tempImageData.data, 640, 480, img_u8);
 
-        // Make image grayscale for easier processing
-        jsfeat.imgproc.grayscale(tempImageData.data, TOTAL_WIDTH, TOTAL_HEIGHT, img_u8);
+      // warp perspective
+      jsfeat.imgproc.warp_perspective(img_u8, img_u8_warp, transform, 0);
 
-        // warp perspective
-        jsfeat.imgproc.warp_perspective(img_u8, img_u8_warp, transform, 0);
-
-        // render result back to canvas
-        var data_u32 = new Uint32Array(tempImageData.data.buffer);
-        var alpha = (0xff << 24);
-        var i = img_u8_warp.cols*img_u8_warp.rows, pix = 0;
-        while(--i >= 0) {
-            pix = img_u8_warp.data[i];
-            data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
-        }
-        initialZoomedContext.putImageData(tempImageData, 0, 0);
-        
-        zoomedContext.drawImage(initialZoomedCanvas.current,
-          START_TOP_LEFT, 0, ZOOMED_WIDTH, BOTTOM_ZOOMED_HEIGHT,
-          0, 0, ZOOMED_WIDTH, BOTTOM_ZOOMED_HEIGHT);
-        const imageData = zoomedContext.getImageData(0, 0, ZOOMED_WIDTH, BOTTOM_ZOOMED_HEIGHT);
+      // render result back to canvas
+      var data_u32 = new Uint32Array(tempImageData.data.buffer);
+      var alpha = (0xff << 24);
+      var i = img_u8_warp.cols*img_u8_warp.rows, pix = 0;
+      while(--i >= 0) {
+          pix = img_u8_warp.data[i];
+          data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
+      }
+      offscreenContext.putImageData(tempImageData, 0, 0);
+      if (offscreenCanvas && offscreenCanvas.current) {
+        zoomedContext.drawImage(offscreenCanvas.current,
+          START_TOP_LEFT, 0, ZOOMED_WIDTH, BOTTOM_WARPED_HEIGHT,
+          0, 0, ZOOMED_WIDTH, BOTTOM_WARPED_HEIGHT);
+        const imageData = zoomedContext.getImageData(0, 0, ZOOMED_WIDTH, BOTTOM_WARPED_HEIGHT);
         onProcessVector(converter?.convertFrame(imageData) || { x: 0, y: 0 })  
       }
     }
@@ -142,9 +110,9 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
       // x2, y2 -> x2', y2'
       640, 0, START_TOP_RIGHT, 0,
       // x3, y3 -> x3', y3'
-      640, 480, 640, BOTTOM_ZOOMED_HEIGHT,
+      640, 480, 640, BOTTOM_WARPED_HEIGHT,
       // x4, y4 -> x4', y4'
-      0, 480, 0, BOTTOM_ZOOMED_HEIGHT
+      0, 480, 0, BOTTOM_WARPED_HEIGHT
     );
 
     jsfeat.matmath.invert_3x3(tempTransform, tempTransform);
@@ -162,21 +130,12 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
   return (
     <>
       <canvas
-        id="initial-zoomed-canvas"
-        style={{
-          display: 'none'
-        }}
-        width={TOTAL_WIDTH}
-        height={TOTAL_HEIGHT}
-        ref={initialZoomedCanvas}
-      />
-      <canvas
         id="offscreen-canvas"
         style={{
           display: 'none'
         }}
-        width={TOTAL_WIDTH}
-        height={TOTAL_HEIGHT}
+        width={width}
+        height={height}
         ref={offscreenCanvas}
       />
       <canvas
@@ -185,7 +144,7 @@ const Processor = ({ video, height, width, onProcessVector }: ProcessorProps) =>
           display: 'none'
         }}
         width={ZOOMED_WIDTH}
-        height={BOTTOM_ZOOMED_HEIGHT}
+        height={BOTTOM_WARPED_HEIGHT}
         ref={zoomedCanvas}
       />
     </>
